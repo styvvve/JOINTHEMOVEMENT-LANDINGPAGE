0ce979cfd5daec2e7743e73ccad529b099b08928
styvvve <styvvvenguele@icloud.com>
2025-06-02 09:44:52 +0200

    Refonte navbar

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ window.addEventListener('DOMContentLoaded', () => {
     //je sais pas encore
 }); 
 
+//menu hamburger
+const menuHamburger = document.querySelector(".menu-hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+menuHamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("mobile-menu");
+})
+
 //on change le header quand on scroll
-window.addEventListener('scroll', () => {
+/*window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     if (window.scrollY > 0) {
         header.classList.add('scrolled'); 
     }else {
         header.classList.remove('scrolled');
     }
-}); 
+}); */
